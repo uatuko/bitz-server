c104695654e58ef6fced7f04b41e44b392e308de
Uditha Atukorala <fake@thewireframe>
2012-10-07 13:53:27 +0100

    skeleton for the main executable (bitz-server)

---
+++ src/bitz-server.cpp
@@ -0,0 +1,18 @@
+/*
+ * bitz-server.cpp
+ *
+ *  Created on: Oct 7, 2012
+ *      Author: Uditha Atukorala
+ */
+
+#include <config.h>
+#include <iostream>
+
+int main() {
+
+	std::cout << PACKAGE_STRING << std::endl;
+
+	// TODO
+
+	return 0;
+}
