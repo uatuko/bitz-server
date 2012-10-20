6ce6abbc7b48435b254f55890443e44f2ce91f2b
Uditha Atukorala <fake@thewireframe>
2012-10-20 23:38:51 +0100

    adding exception classes

---
+++ src/bitz/exception.cpp
@@ -0,0 +1,40 @@
+/*
+ *	bitz-server, An ICAP server implementation in C++
+ *	Copyright (C) 2012	Uditha Atukorala
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "exception.h"
+#include <cerrno>          // For errno
+#include <cstring>         // For strerror
+
+namespace bitz {
+
+	Exception::Exception( const std::string &message, bool inclSystemMessage ) throw() : _message( message ) {
+		if ( inclSystemMessage ) {
+			_message.append( " : " );
+			_message.append( strerror( errno ) );
+		}
+	}
+
+	Exception::~Exception() throw() { }
+
+	const char * Exception::what() const throw() {
+		return _message.c_str();
+	}
+
+} // end of namespace bitz
+
---
+++ src/bitz/manager_exception.h
@@ -0,0 +1,36 @@
+/*
+ *	bitz-server, An ICAP server implementation in C++
+ *	Copyright (C) 2012	Uditha Atukorala
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef BITZ_MANAGER_EXCEPTION_H
+#define BITZ_MANAGER_EXCEPTION_H
+
+#include "exception.h"
+
+namespace bitz {
+
+	class ManagerException: public Exception {
+		public:
+			ManagerException( const std::string &message, bool inclSystemMessage = false ) throw();
+			virtual ~ManagerException() throw();
+	};
+
+} // end of namespace bitz
+
+#endif /* BITZ_MANAGER_EXCEPTION_H */
+
