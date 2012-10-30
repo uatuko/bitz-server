0c08f4e2837be711238bfc6c0c704868049ecb5c
Uditha Atukorala <fake@thewireframe>
2012-10-30 13:33:05 +0000

    touching request handlers and modifiers

---
+++ src/bitz/modifier.h
@@ -0,0 +1,37 @@
+/*
+ *	bitz-server, An ICAP server implementation in C++
+ *	Copyright (C) 2012	Uditha Atukorala
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef BITZ_MODIFIER_H
+#define BITZ_MODIFIER_H
+
+namespace bitz {
+
+	class Modifier {
+	public:
+		Modifier();
+		virtual ~Modifier();
+
+	private:
+
+	};
+
+} /* end of namespace bitz */
+
+#endif /* !BITZ_MODIFIER_H */
+
