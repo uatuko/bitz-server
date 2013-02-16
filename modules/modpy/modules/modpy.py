3611b609d33479d8ff49f27e65076da9c1fa8b26
Uditha Atukorala <fake@thewireframe>
2013-02-16 14:12:12 +0000

    basic python interface module

---
+++ modules/modpy/modules/modpy.py
@@ -0,0 +1,17 @@
+#
+# modpy.py
+# Copyright (c) 2013 Uditha Atukorala
+#
+
+def init():
+	print "init() called"
+
+def cleanup():
+	print "cleanup() called"
+	
+def preview():
+	print "preview() called"
+	
+def modify():
+	print "modify() called"
+
