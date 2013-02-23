fe760e5d265bf072a046b413682ef9beca52ab8b
Uditha Atukorala <fake@thewireframe>
2013-02-23 01:37:15 +0000

    expanding tests

--- test/icap-client.py
+++ test/icap-client.py
@@ -48,5 +48,44 @@ sock.close()
 
 print string
 
+# POST
+try:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+	sys.stderr.write("[ERROR] %s\n" % msg[1])
+	sys.exit(1)
+
+try:
+	sock.connect((HOST, PORT))
+except socket.error, msg:
+	sys.stderr.write("[ERROR] %s\n" % msg[1])
+	sys.exit(2)
+
+sock.send( "REQMOD %s ICAP/1.0\r\n" % ( SERVICE ) )
+sock.send( "Host: %s\r\n" % ( HOST ) )
+sock.send( "Encapsulated: req-hdr=0, req-body=147\r\n" )
+sock.send( "\r\n" )
+
+sock.send( "POST /origin-resource/form.pl HTTP/1.1\r\n" )
+sock.send( "Host: www.origin-server.com\r\n" )
+sock.send( "Accept: text/html, text/plain\r\n" )
+sock.send( "Accept-Encoding: compress\r\n" )
+sock.send( "Pragma: no-cache\r\n" )
+sock.send( "\r\n" )
+sock.send( "1e\r\n" )
+sock.send( "I am posting this information.\r\n" )
+sock.send( "0\r\n" )
+sock.send( "\r\n" )
+
+data = sock.recv(1024)
+string = ""
+while len(data):
+	string = string + data
+	data = sock.recv(1024)
+sock.close()
+
+print string
+
+
 sys.exit(0)
 
