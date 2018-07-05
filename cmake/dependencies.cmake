90514ae5173b770b5ad653be5c1fd3162c5dd4fe
Uditha Atukorala <ua@nuked.zone>
2018-07-05 16:17:11 +0100

    make spdlog an external dependency (breaks autotools builds)

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,32 @@
+include(FindPkgConfig)
+include(ExternalProject)
+
+set_directory_properties(PROPERTIES
+	EP_PREFIX ${CMAKE_SOURCE_DIR}/external
+)
+
+
+pkg_check_modules(psocksxx REQUIRED psocksxx>=0.0.6)
+pkg_check_modules(libconfig REQUIRED libconfig++>=1.4)
+pkg_check_modules(python python>=2.7)
+pkg_check_modules(spdlog spdlog>=0.17.0)
+
+
+add_library(spdlog INTERFACE IMPORTED)
+if(NOT spdlog_FOUND)
+	externalproject_add(gabime_spdlog
+		GIT_REPOSITORY     https://github.com/gabime/spdlog.git
+		GIT_TAG            v0.17.0
+		CONFIGURE_COMMAND  ""
+		BUILD_COMMAND      ""
+		INSTALL_COMMAND    ""
+		TEST_COMMAND       ""
+		UPDATE_COMMAND     ""
+	)
+
+	externalproject_get_property(gabime_spdlog source_dir)
+	set(spdlog_INCLUDE_DIRS ${source_dir}/include)
+
+	add_dependencies(spdlog gabime_spdlog)
+endif()
+
